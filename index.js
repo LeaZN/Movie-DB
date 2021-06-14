2899d8e1fbe0696852a76dc90c5137cb96ce09b6
LeaZN <zoueinlea@gmail.com>
2021-06-14 11:19:34 +0300

    Step_3

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ var express = require("express");
 
 var app = express();
 
+
+
 app.get("/", (req, res)=> {
     res.send("ok");
 });
+app.get("/test", (req, res)=> {
+    res.send("status:200, message:ok");
+});
+
+
+var today = new Date();
+var Time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+app.get('/time', (req, res) => {
+
+    res.send({
+        status: 200,
+        message: Time
+    })
+});
+
+
+
+
 
 
 app.listen(3000);
