3899cf3a70b9237886a35f61291212cc3e9c41ba
LeaZN <zoueinlea@gmail.com>
2021-06-14 19:13:00 +0300

    Step_4

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var app = express();
 app.get("/", (req, res)=> {
     res.send("ok");
 });
+
+
 app.get("/test", (req, res)=> {
     res.send("status:200, message:ok");
 });
@@ -21,6 +23,55 @@ app.get('/time', (req, res) => {
         message: Time
     })
 });
+
+
+
+
+
+app.get('/hello/:id', (req, res) => {
+    var id = req.params.id;
+    if (id !== undefined) {
+        res.send({
+            status: 200,
+            message: "Hello," + id
+        })
+    }
+    else {
+        res.send({
+            status: 200,
+            message: "Hello,"
+        })
+    }
+
+})
+;
+
+
+
+
+
+app.get('/search', (req, res) => {
+    var search = req.query.s;
+    if (search !== undefined) {
+        res.send({
+            status:200, 
+            message:"ok", 
+            data: search
+        })
+    }
+    else {
+        res.send({
+            status:500, 
+            error:true, 
+            message:"you have to provide a search"
+        })
+    }
+
+})
+;
+
+
+
 
 
 
